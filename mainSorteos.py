281d84a0418d68dd1f2319578ed00228f81a56b9
mrferu <fernando.hernandez41@unach.mx>
2022-10-22 21:25:34 -0500

    DE_Test: intialize menu

---
+++ mainSorteos.py
@@ -0,0 +1,25 @@
+print("SORTEOS DEL BIENESTAR".center(100,"="))
+while True:
+
+    ## menu here
+
+    select_option = int(input("Seleccione una opcion:\t"))
+    match select_option:
+        case 1:
+            # clients register 
+
+        case 2:
+            # comb register
+
+        case 3:
+            # comb generator
+
+        case 4:
+            # ticket generator
+        case 5:
+            # Loto 
+        case 6:
+            # check winners
+        case 7:
+            print("Goodbye".center(100,"~"))
+            break
