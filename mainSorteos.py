d8749d1e927645b3d16ca513395d95270f4d7bbe
mrferu <fernando.hernandez41@unach.mx>
2022-10-22 21:41:47 -0500

    DE_Test: add functions to validate and gen a ticket - Add loto [WIP] - Add price checker to get the ticket's price

--- mainSorteos.py
+++ mainSorteos.py
@@ -75,8 +75,108 @@ def combRandom():
         else:
             continue
 
+def priceChecker(size_ticket):
+    match size_ticket:
+        case 6:
+            return 15
+        case 7:
+            return 105
+        case 8:
+            return 420
+        case 9:
+            return 1260
+        case 10:
+            return 3150
 
-print("SORTEOS DEL BIENESTAR".center(100,"="))
+def genTicket():
+    print("Generando boleto".center(100,"_"))
+    print("se usaran los siguientes datos...".center(100))
+    id_rfc = input("RFC del cliente:\t")
+    f = open('data_base.json','r')
+    a = f.read()
+    db = json.loads(a)
+    size = len(db[id_rfc]['ticket'])
+
+
+    print(f"RFC:\t{id_rfc}")
+    print(f"Nombre(s):\t{db[id_rfc]['c_name']}")
+    print(f"Apellido(s):\t{db[id_rfc]['c_lname']}")
+    print(f"No. de combinaciones:\t{size}")
+    print(f"Precio del boleto:\t{priceChecker(size)}")
+
+    print(f"\nSu boleto se guardo en el archivo {id_rfc}.txt para que pueda imprimirlo")
+
+    crear = open(f"{id_rfc}.txt", "w")#Se abre el archivo
+    crear.write("*------*".center(55," ")  + "\n")
+    crear.write("SORTEO".center(55, " ") + "\n")
+    crear.write("*------*".center(55," ")  + "\n")
+    crear.write("\n")
+    crear.write("Fecha: 25/10/2022" + "Hora: 10:00".center(57, " ") + "\n")
+    crear.write("\n")
+    crear.write("+---+".center(55," ")  + "\n")
+    crear.write("|RFC|".center(55, " ") + "\n")
+    crear.write("+---+".center(55," ")  + "\n")
+    crear.write(str(id_rfc).center(55, " ") + "\n")
+    crear.write("\n")
+    crear.write("+------+".center(55," ")  + "\n")
+    crear.write("|NOMBRE|".center(55, " ") + "\n")
+    crear.write("+------+".center(55," ")  + "\n")
+    crear.write(str(db[id_rfc]['c_name']) + str(db[id_rfc]['c_lname']).center(55, " ") + "\n")
+    crear.write("\n")
+    crear.write("+--------------+".center(55," ")  + "\n")
+    crear.write("|No.Combinacion|".center(55, " ") + "\n")
+    crear.write("+--------------+".center(55," ")  + "\n")
+    crear.write(str(size).center(55, " ") + "\n")
+    crear.write("\n")
+    crear.write("+---------+".center(55," ")  + "\n")
+    crear.write("|No.Sorteo|".center(55, " ") + "\n")
+    crear.write("+---------+".center(55," ")  + "\n")
+    crear.write(str(db[id_rfc]["ticket"]).center(55, " ") + "\n")
+    crear.write("\n")
+    crear.write("+-----+".center(55," ")  + "\n")
+    crear.write("Precio".center(55, " ") + "\n")
+    crear.write("+-----+".center(55," ")  + "\n")
+    crear.write(str(priceChecker(size)).center(55, " ") + "\n")
+    f.close()
+    crear.close() #Se cierra el archivo creado
+
+def genLoto():
+    print("Generando sorteo".center(100,"_"))
+
+    numbers = "0123456789"
+    size = int(input("Introduzca el tamaño del boleto ganandor (min. 6 | max. 10):\t"))
+
+    count_limit = 0
+    while True:
+        temp = "".join(random.sample(numbers,2))
+        if int(temp) < 59:
+            winner_ticket.append(int(temp))
+            count_limit += 1
+            if count_limit == size:
+                break
+            else:
+                continue
+        else:
+            continue
+    print(str(winner_ticket))
+
+def ticketChecker():
+    print("Validando boleto".center(100,"_"))
+    size = int(input("Introduzca el tamaño del boleto ganandor (min. 6 | max. 10):\t"))
+    ticket_temp = input("Inserte sus numeros separados por comas:\t")
+    ticket_to_check = ticket_temp.split(',')
+    
+    count_limit = 0
+    for number in range(size):
+        if ticket_to_check[number] == winner_ticket[number]:
+            count_limit += 1
+    if count_limit == size:
+        print(f"{ticket_to_check} es ganador")
+    else:
+        print(f"{ticket_to_check} no es ganador")
+
+
+print("SORTEOS APP".center(100,"="))
 while True:
 
     showMenu() # menu here
@@ -91,15 +191,14 @@ while True:
             combRegister()
 
         case 3:
-            # comb generator
-            combRandom()
+            combRandom() # comb generator
 
         case 4:
-            # ticket generator
+            genTicket()# ticket generator
         case 5:
-            # Loto 
+            genLoto() # Loto 
         case 6:
-            # check winners
+            ticketChecker() # check winners
         case 7:
             print("Goodbye".center(100,"~"))
             break
