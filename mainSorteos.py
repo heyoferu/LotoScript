eb88f8ae1634065a52c1e88679481ccb39777773
mrferu <fernando.hernandez41@unach.mx>
2022-10-22 21:34:41 -0500

    DE_Test: add fuction to load new clients in json file - import json - add dict to manage clients

--- mainSorteos.py
+++ mainSorteos.py
@@ -1,14 +1,41 @@
-import menu
+import json
+
+clients_db = {}
+
+
+def update_db():
+    jsonfile = open("data_base.json","w")
+    json_temp = json.dumps(clients_db)
+    jsonfile.write(json_temp)
+    jsonfile.close()
+
+def showMenu():
+    menu = ["Capturar cliente","Registrar combinancion","Generar combinacion aleatoria","Generar boleto","Generar sorteo","Validar boleto","Salir"]
+    for option in range(len(menu)):
+            print(f"({option+1}) {menu[option]}")
+
+def addClient():
+    print("Capturarando cliente".center(100,"_"))
+    id_rfc = input("RFC del cliente:\t")
+    names = input("Nombre(s) del cliente:\t")
+    lnames = input("Apellido(s) del cliente:\t")
+
+    clients_db[id_rfc] = {}
+    clients_db[id_rfc]["c_name"] = names
+    clients_db[id_rfc]["c_lname"] = lnames
+    update_db()
+
+
 print("SORTEOS DEL BIENESTAR".center(100,"="))
 while True:
 
-    ## menu here
+    showMenu() # menu here
 
     select_option = int(input("Seleccione una opcion:\t"))
     match select_option:
         case 1:
-            menu.showMenu()# clients register 
-
+            # clients register 
+            addClient()
         case 2:
             # comb register
 
--- menu.py
+++
@@ -1,4 +0,0 @@
-def showMenu():
-    menu = ["Capturar cliente","Registrar combinancion","Generar combinacion aleatoria","Generar boleto","Generar sorteo","Validar boleto","Salir"]
-    for option in range(len(menu)):
-            print(f"({option+1}) {menu[option]}")
